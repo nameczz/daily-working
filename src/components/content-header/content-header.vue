7e39bacee91c09e5d6c2be95d93221da392724de
nameczz <nameczz@126.com>
2017-04-25 23:36:36 +0800

    better scroll用来move日历
    安装better scroll第三方插件, 调试其属性达到预期效果。

--- src/components/content-header/content-header.vue
+++ src/components/content-header/content-header.vue
@@ -5,16 +5,19 @@
         <td v-for="day in week" :width="tdWidth">{{ day }}</td>
       </tr>
     </table>
-    <ul class="calendar-wrapper" id="calendar-wrapper">
-      <li v-for="month in year" class="month">
-        <calendar :td-width="tdWidth" :month="month"></calendar>
-      </li>
-    </ul>
+    <div ref="calendarWrapper">
+      <ul class="calendar-wrapper" id="calendar-wrapper" >
+        <li v-for="month in year" class="month">
+          <calendar :td-width="tdWidth" :month="month"></calendar>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 import calendar from 'components/calendar/calendar';
+import BScroll from 'better-scroll';
 
 export default {
   props: {
@@ -28,8 +31,15 @@ export default {
   data () {
     return {
       week: ['日', '一', '二', '三', '四', '五', '六'],
-      year: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+      year: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      oldPos: 0,
+      endPos: 0
     };
+  },
+  created () {
+    this.$nextTick(() => {
+      this._initScroll();
+    });
   },
   mounted () {
     this.wrapperWidth;
@@ -52,6 +62,33 @@ export default {
       let width = this.width * 12;
       let calendarBox = document.getElementById('calendar-wrapper');
       calendarBox.style.width = `${width}px`;
+    }
+  },
+  methods: {
+    // make calendar move
+    _initScroll () {
+      this.calendarScroll = new BScroll(this.$refs.calendarWrapper, {
+        startX: -this.width * this.$store.state.month + this.$store.state.month + 1,
+        scrollX: true,
+        probeType: 3
+      });
+
+      this.calendarScroll.on('scrollStart', () => {
+        this.oldPos = this.calendarScroll.startX;
+      });
+
+      this.calendarScroll.on('scroll', (pos) => {
+        this.endPos = pos.x;
+      });
+
+      this.calendarScroll.on('scrollEnd', () => {
+        let moveLine = Math.floor(this.width / 2);
+        console.log(moveLine);
+        console.log(Math.abs(this.oldPos - this.endPos));
+        if (Math.abs(this.oldPos - this.endPos) < moveLine) {
+          this.calendarScroll.scrollTo(this.oldPos, this.calendarScroll.startY);
+        }
+      });
     }
   },
   components: {
